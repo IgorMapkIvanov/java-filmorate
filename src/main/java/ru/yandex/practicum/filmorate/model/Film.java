5c6dc4bbe0bba185f12d60c49b4ca4c527486a50
Igor Ivanov <mapk01101982@yandex.ru>
2022-07-11 23:39:26 +0300

    feat: add Film.class and User.class i

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+
+@Entity
+@Data
+public class Film {
+    @Id
+    @GeneratedValue
+    private long id;
+    @NotBlank(message = "название фильма не должно быть пустым")
+    private String name;
+    @Size(max = 200, message = "описание фильма не должно быть больше 200 символов")
+    private String description;
+    @NotNull(message = "дата выхода не должна быть пустой")
+    @Min(value = -2335564800L, message = "дата выхода фильма не должна быть ранее 28 декабря 1895г")
+    private LocalDate releaseDate;
+    @NotNull(message = "длительность фильма не должна быть пустой")
+    @Positive(message = "длительность фильма должна быть больше 0")
+    private Duration duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Email
+    private String email;
+    @NotBlank
+    private String login;
+    private String name;
+    private LocalDate birthday;
+}
