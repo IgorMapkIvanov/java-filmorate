c92442e18989909dec48838045e651c49d5c0033
Igor Ivanov <mapk01101982@yandex.ru>
2022-07-12 19:32:54 +0300

    feat: add controllers for Film and User without validation

--- src/main/java/ru/yandex/practicum/filmorate/web/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/web/controller/FilmController.java
@@ -3,17 +3,19 @@ package ru.yandex.practicum.filmorate.web.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.*;
 
 @RestController
 @RequestMapping(value = "/films")
 @Slf4j
 public class FilmController {
-    private final Map<Long, Film> films = new HashMap<>();
+    private long filmId = 0;
+    private final Map<Long, Film> films = new TreeMap<>(Long::compareTo);
 
     @GetMapping
     public List<Film> getFilms(){
@@ -23,21 +25,30 @@ public class FilmController {
 
     @PostMapping
     public Film addFilm(@Valid @RequestBody Film film){
+        film.setId(generateId());
+        if(film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))){
+            log.info("Дата релиза ({}) не болжна быть раньше 28.12.1985 г.", film.getReleaseDate());
+            throw new ValidationException("Дата релиза не болжна быть раньше 28.12.1985 г.");
+        }
         films.put(film.getId(), film);
         log.info("Новый фильм добавлен: {}", film);
         return films.get(film.getId());
     }
 
-    @PutMapping(value = "/{id}")
-    public Film changeFilm(@NotNull @PathVariable("id") Long id, @Valid @RequestBody Film film){
-        Optional<Film> optionalFilm = Optional.of(films.get(id));
+    @PutMapping
+    public Film changeFilm(@Valid @RequestBody Film film){
+        Optional<Film> optionalFilm = Optional.of(films.get(film.getId()));
         if (optionalFilm.isPresent()) {
             films.put(film.getId(), film);
-            log.info("Фильм с id = {} обновлен.", id);
+            log.info("Фильм с id = {} обновлен.", film.getId());
             return films.get(film.getId());
         } else {
-            log.info("Не удалось обновить фильм с id = {}, фильм с таким id не найденю", id);
-            throw new NotFoundException(String.format("Фильм с id = %s не найден.", id));
+            log.info("Не удалось обновить фильм с id = {}, фильм с таким id не найденю", film.getId());
+            throw new NotFoundException(String.format("Фильм с id = %s не найден.", film.getId()));
         }
+    }
+
+    private long generateId(){
+        return filmId = filmId + 1;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/web/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/web/controller/UserController.java
@@ -1,4 +1,54 @@
 package ru.yandex.practicum.filmorate.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/users")
+@Slf4j
 public class UserController {
+    private long userId = 0;
+    private final Map<Long, User> users = new TreeMap<>(Long::compareTo);
+
+    @GetMapping
+    public List<User> getFilms(){
+        log.info("Список из {} фильмов отправлен.", users.size());
+        return new ArrayList<>(users.values());
+    }
+
+    @PostMapping
+    public User addUser(@Valid @RequestBody User user){
+        user.setId(generateId());
+        if(user.getBirthday().isAfter(LocalDate.now())){
+            log.info("Дата релиза ({}) не болжна быть раньше 28.12.1985 г.", user.getBirthday());
+            throw new ValidationException(String.format("Дата рождения не болжна быть позже $s", LocalDate.now()));
+        }
+        users.put(user.getId(), user);
+        log.info("Новый пользователь добавлен: {}", user);
+        return users.get(user.getId());
+    }
+
+    @PutMapping
+    public User changeUser(@Valid @RequestBody User user){
+        Optional<User> optionalUser = Optional.of(users.get(user.getId()));
+        if (optionalUser.isPresent()) {
+            users.put(user.getId(), user);
+            log.info("Пользователь с id = {} обновлен.", user.getId());
+            return users.get(user.getId());
+        } else {
+            log.info("Не удалось обновить пользователя с id = {}, фильм с таким id не найденю", user.getId());
+            throw new NotFoundException(String.format("Пользователь с id = %s не найден.", user.getId()));
+        }
+    }
+
+    private long generateId(){
+        return userId = userId + 1;
+    }
 }
