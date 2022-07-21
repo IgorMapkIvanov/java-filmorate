5a1ed424c80ceaf90e6c57abe2a561c8b32104d0
Igor Ivanov <mapk01101982@yandex.ru>
2022-07-21 22:43:05 +0300

    feat: add exceptions and ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -1,13 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class NotFoundException extends RuntimeException {
-    String message;
     public NotFoundException(String message) {
-        this.message = message;
+        super(message);
     }
-
-    public String getMessage() {
-        return message;
-    }
-
 }
--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,13 +1,12 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class ValidationException extends RuntimeException {
-    private String message;
 
     public ValidationException(String message) {
         super(message);
     }
 
     public String getMessage() {
-        return message;
+        return super.getMessage();
     }
 }
