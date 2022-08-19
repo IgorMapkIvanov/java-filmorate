1b2bab70999cd31c65354fc2049a4a0137550716
Igor Ivanov <mapk01101982@yandex.ru>
2022-08-19 19:15:31 +0300

    feat: Sprint #12 add event method

--- src/main/java/ru/yandex/practicum/filmorate/dao/DirectorRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/DirectorRepository.java
@@ -68,6 +68,7 @@ public class DirectorRepository {
     }
 
     public boolean delete(Integer id) {
+        filmDirectorsRepository.deleteFilmDirectors(id);
         String sql = "DELETE FROM DIRECTORS WHERE ID = ?";
         return jdbcTemplate.update(sql, id) > 0;
     }
