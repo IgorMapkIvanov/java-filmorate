963f14afb7438265cb95a38425f40fec92d06855
Кирилл <teador2022@gmail.com>
2022-08-27 09:09:59 +0500

    add-recommendations

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendService.java
@@ -27,18 +27,24 @@ public class RecommendService {
         Map<Long, HashMap<Long, Double>> data = recommendDbRepository.createMatrixOfDiff();
         Map<Long, Double> userLike = data.get(userId);
 
+        /** На основе имеющихся данных мы рассчитаем отношения между элементами, а также количество вхождений элементов.
+         *  Для каждого пользователя мы проверяем его/ее рейтинг предметов: */
         for (Map.Entry<Long, HashMap<Long, Double>> entryData : data.entrySet()) {
             if (entryData.getKey() == userId) {
                 continue;
             }
+            /**На следующем шаге мы проверяем, существует ли элемент в наших матрицах.
+             * Если это первый случай, мы создаем новую запись в картах: */
             matrixOfDiff.put(entryData.getKey(), new HashMap<Long, Double>());
 
+            /** На следующем шаге мы собираемся сравнить рейтинги всех элементов: */
             for (Map.Entry<Long, Double> entryData2 : entryData.getValue().entrySet()) {
                 double diff = userLike.get(entryData2.getKey()) * entryData2.getValue();
                 if (diff == 1.0) {
                     if (!matrixOfFreq.containsKey(entryData.getKey())) {
                         matrixOfFreq.put(entryData.getKey(), 0);
                     }
+                    /** Если кто-то уже оценил элемент ранее, мы увеличиваем количество частот на единицу. */
                     int count = matrixOfFreq.get(entryData.getKey());
                     count++;
                     matrixOfFreq.put(entryData.getKey(), count);
@@ -48,12 +54,15 @@ public class RecommendService {
 
         }
 
+        /** Определяем фильмы, которые один пролайкал, а другой нет. */
         for (Map.Entry<Long, HashMap<Long, Double>> entryData : data.entrySet()) {
             if (matrixOfFreq.get(entryData.getKey()) == null
                     || matrixOfFreq.get(entryData.getKey()) == 0
                     || entryData.getKey() == userId) {
                 continue;
             }
+            /** Рекомендовать фильмы, которым поставил лайк пользователь с похожими вкусами,
+             * а тот, для кого составляется рекомендация, ещё не поставил.: */
             for (Map.Entry<Long, Double> entryData2 : entryData.getValue().entrySet()) {
                 Long ed2 = entryData2.getKey();
                 Long ed1 = entryData.getKey();
